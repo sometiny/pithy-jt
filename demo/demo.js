1fdcce2a80afeca5f043caf7515da58de996a856
Anlige <zhanghuiguoanlige@126.com>
2017-08-08 14:11:03 +0800

    增加htmltag示例；

--- demo/demo.js
+++ demo/demo.js
@@ -12,7 +12,8 @@ demo
 		'<a href="../index.html">在线测试工具</a> '+
 		'<a href="index.html">基本示例</a> '+
 		'<a href="htmlhelper.html">PjtHtmlhelper</a> '+
-		'<a href="extends.html">PjtExtends</a>';
+		'<a href="extends.html">PjtExtends</a> ' +
+		'<a href="htmltag.html">Html标签模式的模板兼容扩展</a>';
 	}
 	
 	on(window, 'load', __initlize);
